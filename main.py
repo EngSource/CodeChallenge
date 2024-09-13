7e5e429a17943ae2751bfa0f996928eccddb8ca7
EngSource <jwroiko@gmail.com>
2024-09-13 14:40:45 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#This is a Github Test of loading the json file
+
+import json
+
+#loads the json formatting file
+def load_file(formatting_file):
+    with open(formatting_file, 'r') as f:
+        return json.load(f)
+
+#Use raw string
+json_file_path = r"C:\Users\jwroi\Downloads\imandra-technical-interview-1.0.2\data\tags.json"
+
+#prints json file
+print(load_file(json_file_path))
