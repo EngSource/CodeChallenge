a6d66d276d81a4e0239e36ed09e9a46ac6533c95
EngSource <jwroiko@gmail.com>
2024-09-13 15:06:39 -0500

    Adding fix.log
    - Adding fix.log file to main.py
    - Splitting the FIX packets using the '|' symbol used in the fix.log file

--- main.py
+++ main.py
@@ -3,12 +3,27 @@
 import json
 
 #loads the json formatting file
-def load_file(formatting_file):
-    with open(formatting_file, 'r') as f:
-        return json.load(f)
+def load_json_file(formatting_file):
+    with open(formatting_file, 'r') as json_file:
+        return json.load(json_file)
 
-#Use raw string
-json_file_path = r"C:\Users\jwroi\Downloads\imandra-technical-interview-1.0.2\data\tags.json"
+#loads the FIX file and splits it into packets
+def load_fix_file(data_file):
+    with open(data_file, 'r') as fix_file:
+        return fix_file.read().splitlines()
+
+
+#Reference local json file
+json_file_path = "tags.json"
 
 #prints json file
-#print(load_file(json_file_path))
+#print(load_json_file(json_file_path))
+
+#Reference local fix data file
+fix_file_path = "fix.log"
+
+log = load_fix_file(fix_file_path)
+
+packets = [packet.split('|') for packet in log]
+
+print(packets)
